94a761ab0ff2e6c5163fd41ff6ccb4a15d7730a4
MymenTech <mymentech@gmail.com>
2018-11-19 15:14:59 +0600

    Added image id to product url

--- woocommerce/views/generated-product-item.php
+++ woocommerce/views/generated-product-item.php
@@ -13,14 +13,14 @@
                 <?php echo class_exists('YITH_WCWL') ? do_shortcode('[yith_wcwl_add_to_wishlist product_id="'.$pid.'"]') : ''; ?>
 
                 <?php /*quick view button*/ ?>
-                <a href="<?php echo get_permalink($pid); ?>" data-id="<?php echo esc_attr($pid); ?>" class="quick-view-btn" aria-label="<?php esc_attr_e('Show in Quickview', 'minera'); ?>">
+                <a href="<?php echo get_permalink($pid)."?mt-image-id={$_gid}"; ?>" data-id="<?php echo esc_attr($pid); ?>" class="quick-view-btn" aria-label="<?php esc_attr_e('Show in Quickview', 'minera'); ?>">
                     <span class="p-tooltip"><?php esc_html_e('Show in Quickview', 'minera'); ?></span>
                 </a>
 
                 <?php /*add to Cart button*/ ?>
                 <span class="p-box-atc">
                     <?php if($variation): ?>
-                        <a href="<?php echo get_permalink($pid); ?>" class="select-option-btn ht-ico-cart" aria-label="<?php esc_attr_e('Select Options', 'minera'); ?>">
+                        <a href="<?php echo get_permalink($pid)."?mt-image-id={$_gid}"; ?>" class="select-option-btn ht-ico-cart" aria-label="<?php esc_attr_e('Select Options', 'minera'); ?>">
                             <span class="p-tooltip"><?php esc_html_e('Select Options', 'minera'); ?></span>
                         </a>
                     <?php else: ?>
@@ -31,7 +31,7 @@
                 </span>
             </div>
             <?php /*product image*/ ?>
-            <a href="<?php echo get_permalink($pid); ?>" class="p-image" aria-label="<?php esc_attr_e('Product image', 'minera'); ?>" data-ori_src="<?php echo esc_attr($origin_src[0]); ?>">
+            <a href="<?php echo get_permalink($pid)."?mt-image-id={$_gid}"; ?>" class="p-image" aria-label="<?php esc_attr_e('Product image', 'minera'); ?>" data-ori_src="<?php echo esc_attr($origin_src[0]); ?>">
                 <img src="<?php echo esc_url($img_src[0]); ?>" alt="<?php echo esc_attr($img_alt); ?>">
                 <?php if(!empty($hover_src)): ?>
                     <span class="p-img-hover" style="background-image: url(<?php echo esc_url($hover_src[0]); ?>)"></span>
@@ -39,20 +39,19 @@
             </a>
         </div>
         <div class="p-content">
-            <a class="p-title" href="<?php echo get_permalink($pid); ?>" aria-label="<?php esc_attr_e('Product title', 'minera'); ?>"><?php echo get_the_title($pid); ?></a>
+            <a class="p-title" href="<?php echo get_permalink($pid)."?mt-image-id={$_gid}"; ?>" aria-label="<?php esc_attr_e('Product title', 'minera'); ?>"><?php echo get_the_title($pid); ?></a>
             <div class="price" aria-label="<?php esc_attr_e('Product price', 'minera'); ?>"><?php echo wp_kses_post($p_price); ?></div>
             <?php minera_swatches_list();/*swatches color*/ ?>
 
             <?php /*list style: add to Cart button*/ ?>
             <div class="atc-shop-list">
                 <?php if($variation): ?>
-                    <a href="<?php echo get_permalink($pid); ?>" class="atc-select-option-btn" aria-label="<?php esc_attr_e('Select Options', 'minera'); ?>"><?php esc_html_e('Select Options', 'minera'); ?></a>
+                    <a href="<?php echo get_permalink($pid)."?mt-image-id={$_gid}"; ?>" class="atc-select-option-btn" aria-label="<?php esc_attr_e('Select Options', 'minera'); ?>"><?php esc_html_e('Select Options', 'minera'); ?></a>
                 <?php else: ?>
-                    <a href="<?php echo esc_url($atc_url); ?>" data-quantity="1" data-product_id="<?php echo esc_attr($pid); ?>" class="add_to_cart_button ajax_add_to_cart" aria-label="<?php esc_attr_e('Add to Cart', 'minera'); ?>"><?php esc_html_e('Add to Cart', 'minera'); ?></a>
+                    <a href="<?php echo esc_url($atc_url)."?mt-image-id={$_gid}"; ?>" data-quantity="1" data-product_id="<?php echo esc_attr($pid); ?>" class="add_to_cart_button ajax_add_to_cart" aria-label="<?php esc_attr_e('Add to Cart', 'minera'); ?>"><?php esc_html_e('Add to Cart', 'minera'); ?></a>
                 <?php endif; ?>
             </div>
         </div>
         <?php minera_swatches_list();/*swatches color*/ ?>
     </div>
 </li>
-
